e0a7e4a82e080a14a2fa57cb18bcffdc869490ff
Chris Jordan <crobertjordan@yahoo.com>
2017-09-05 06:36:26 -0600

    changed up webpack config, removed a plugin for a api key"

--- webpack.config.js
+++ webpack.config.js
@@ -38,9 +38,6 @@ module.exports = {
     new ExtractTextPlugin('style.css', { allChunks: true }),
     new webpack.DefinePlugin({
     'MAP_API_KEY': JSON.stringify(process.env.MAP_API_KEY),
-    'WEATHER_API_KEY':JSON.stringify(process.env.WEATHER_API_KEY)
-    'GOOGLE_API':JSON.stringify(process.env.GOOGLE_API)
-
   })
  ]
 }
